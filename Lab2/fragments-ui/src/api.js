576356ed4d4b9071abe090e26f96fc8455dfb72e
Diba Makki <sdmakki@myseneca.ca>
2025-09-16 15:57:02 -0400

    files has been moved

---
+++ Lab2/fragments-ui/src/api.js
@@ -0,0 +1,13 @@
+//src/api.js
+
+const apiUrl = process.env.API_URL || "http://localhost:8080";
+
+export async function getUserFragments(user) {
+  const response = await fetch(`${apiUrl}/v1/fragments`, {
+    headers: user.authorizationHeaders(),
+  });
+  if (!response.ok) {
+    throw new Error(`Failed to get fragments: ${response.status}`);
+  }
+  return await response.json();
+}
